d23aa59d4f2e8d27cc1b48dbc8ef580d5b4ab4f2
evuschka <evangelina_lapshova@mail.ru>
2026-03-12 09:07:20 +0300

    Update caesar.h

--- caesar.h
+++ caesar.h
@@ -1,13 +1,11 @@
-просили в задании.
 #ifndef CAESAR_H
 #define CAESAR_H
 
-// Объявляем функции с атрибутом extern для экспорта
+
 extern char* caesar_encrypt(const char* text, int shift);
 extern char* caesar_decrypt(const char* text, int shift);
 
-// Вспомогательная функция для очистки памяти из Python,
-// чтобы избежать утечек памяти (требование задания).
+
 extern void free_result(char* str);
 
 #endif
